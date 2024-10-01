efdd98d0f4a7c1a64636174170a658b092bb6413
Jason Neumark Mickela <jason@rootid.com>
2024-10-01 13:28:40 -0700

    initial commit

---
+++ src/Plugin/Field/FieldFormatter/VerticalTimelineFormatter.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Drupal\radicati_component_timeline\Plugin\Field\FieldFormatter;
+
+use Drupal\Core\Field\FieldDefinitionInterface;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\FormatterBase;
+
+/**
+ * Plugin implementation of the 'vertical_timeline_formatter' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "vertical_timeline_formatter",
+ *   label = @Translation("Vertical Timeline Formatter"),
+ *   field_types = {
+ *     "entity_reference_revisions"
+ *   }
+ * )
+ */
+class VerticalTimelineFormatter extends FormatterBase {
+  public static function isApplicable(FieldDefinitionInterface $field_definition) {
+    return parent::isApplicable($field_definition) && strpos($field_definition->id(), "timeline");
+  }
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $elements = [
+      '#theme' => 'radicati_vertical_timeline',
+      '#items' => [],
+    ];
+    $entities = $items->referencedEntities();
+    $current_group = null;
+
+    // For rendering the paragraph entities.
+    $view_mode = 'default';
+    $view_builder = \Drupal::entityTypeManager()->getViewBuilder('paragraph');
+    $storage = \Drupal::entityTypeManager()->getStorage('paragraph');
+
+    foreach ($entities as $key => $item) {
+      $label = $item->field_timeline_item_label->value;
+
+      if($current_group == null) {
+        $current_group = [
+          '#theme' => 'radicati_vertical_timeline_group',
+          '#label' => $label,
+          '#items' => [],
+        ];
+      }
+
+      $current_group['#items'][] = $view_builder->view($item, $view_mode);
+
+      if($entities[$key+1] == null || $label != $entities[$key+1]->field_timeline_item_label->value) {
+        $elements['#items'][] = $current_group;
+        $current_group = null;
+      }
+    }
+
+
+    return $elements;
+  }
+}
